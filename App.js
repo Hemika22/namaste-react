7c40d4c2455688d5e66aa9f6b9c844e956c2fa03
Myakala <hemika.myakala@acsicorp.com>
2023-12-22 20:03:14 +0530

    first comment

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading=React.createElement('h1',{id: "heading", style: {color:"blue"}},"hello React!");
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
