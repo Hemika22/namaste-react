d8d3f3a6ce8584d22613ab45f3bccccb5bf14a91
Myakala <hemika.myakala@acsicorp.com>
2024-01-07 15:18:36 +0530

    added readme file

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-const heading=React.createElement('h1',{id: "heading", style: {color:"blue"}},"hello React!");
+const heading=React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"},[React.createElement('h1',{id: "heading"},"hello React....!"),React.createElement('h2',{id: "heading"},"h2 React....!")]
+));
+
+
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(heading); // render() will convert this heading object into an h1 tag
