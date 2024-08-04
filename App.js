c73e7f1f677bcdff20b39db2d4a4b1620dea6c77
Myakala <hemika.myakala@acsicorp.com>
2024-08-04 13:26:17 +0530

    episode-01

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-const heading=React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"},[React.createElement('h1',{id: "heading"},"hello React....!"),React.createElement('h2',{id: "heading"},"h2 React....!")]
-));
+const heading=React.createElement("div", {id:"parent"}, 
+React.createElement("div", {id:"child"},
+[React.createElement('h1',{id: "heading"},"hello React....!"),
+React.createElement('h2',{id: "heading"},"h2 React....!")]
+),React.createElement("h6",{},"H6 React....."));
 
 
 
